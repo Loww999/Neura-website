78a95a36f769de9a305613bb28cc5e62c2c99768
Lowflow <evasmanaid@gmail.com>
2025-12-26 21:31:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const API_STATUS_URL = "http://127.0.0.1:8000/status";
+
+const minigames = ["Survival Vanilla", "SkyWars", "Crystal pvp", "Duels", "Survival economy", "Tnt Run"];
+let index = 0;
+const minigameText = document.getElementById("minigameText");
+
+setInterval(() => {
+  index = (index + 1) % minigames.length;
+  minigameText.textContent = minigames[index];
+}, 1500);
+
+async function updateServerStatus() {
+  const serverStatus = document.getElementById("serverStatus");
+  try {
+    const res = await fetch(API_STATUS_URL);
+    const data = await res.json();
+    serverStatus.textContent = `Status: ${data.online ? "Online" : "Offline"}`;
+  } catch (err) {
+    serverStatus.textContent = "Status: Error";
+  }
+}
+
+updateServerStatus();
+setInterval(updateServerStatus, 30000);
